235a2cc98026fe2af09b9c5f1054fd24ba924c56
srira-amirishetty <sriramamirishetty@gmail.com>
2025-06-25 23:20:43 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import {ScrollTrigger,SplitText} from "gsap/all";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 
