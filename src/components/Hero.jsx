9571f610be7cf96e96d691e326949f265a3c5b8e
srira-amirishetty <sriramamirishetty@gmail.com>
2025-06-26 22:15:51 +0530

    build change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -101,7 +101,7 @@ const Hero = () => {
     <div className="video absolute inset-0">
         <video
         ref={videoRef}
-        src="/videos/input.mp4"
+        src="videos/input.mp4"
         muted
         playsInline
         preload="auto"
